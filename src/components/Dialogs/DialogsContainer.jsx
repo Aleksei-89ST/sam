37c8b17c548b3fa7fce7d92a4e565e23c7b308b9
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-04-14 11:34:58 +0400

    настройка диалогов

---
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -0,0 +1,30 @@
+import { onSendMessageCreater, onToggleIsFetching } from '../../redux/dialogs-reducer';
+import Dialogs from './Dialogs';
+import { connect } from 'react-redux';
+import { withAuthRedirect } from '../hoc/withAuthRedirect';
+import { compose } from 'redux';
+
+
+
+const mapStateToProps = (state) => {
+  return {
+    messagesPage: state.messagesPage
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    sendMessage: (newMessageBody) => {
+      dispatch(onSendMessageCreater(newMessageBody));
+    },
+    toggleIsFetching: (isFetching) => {
+      dispatch(onToggleIsFetching(isFetching));
+    }
+  }
+}
+
+
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  withAuthRedirect
+)(Dialogs)
